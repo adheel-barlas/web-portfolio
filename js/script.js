6f55c22cb2128b358b907cc91395da8bb18d8eac
Adheel Barlas <adheelb8@gmail.com>
2018-07-31 20:26:06 +0100

    Adding project files

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Plan:
+  // Create fixed navbar
+  // Navbar appears only the user scrolls up
+
+var beforeScroll = 0;
+var afterScroll;
+var $navbar = $('.navbar');
+
+
+
+
+// find current position
+$(window).on("scroll", function(){
+  
+  // Calculate updated scroll position
+  afterScroll = $(this).scrollTop();
+      
+  // Compare before and after scroll
+  
+  // if scrolling is quick and less then -10
+  if (beforeScroll < afterScroll) {
+    // user is scrolling down
+    $navbar.removeClass('slideDown').addClass('slideUp');
+  } else if (beforeScroll > afterScroll) {
+    // user is scrolling up
+    $navbar.removeClass('slideUp').addClass('slideDown');
+  }
+    
+
+  // Reset beforeScroll to afterScroll position
+  
+  beforeScroll = afterScroll;
+  
+});
