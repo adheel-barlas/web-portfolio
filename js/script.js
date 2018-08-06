6ee3323e1e1ab6563135b3137066f01ccc9615ec
adheel-barlas <adheelb8@gmail.com>
2018-08-06 15:20:28 +0100

    Added skill bars to about me

--- js/script.js
+++ js/script.js
@@ -6,9 +6,6 @@ var beforeScroll = 0;
 var afterScroll;
 var $navbar = $('.navbar');
 
-
-
-
 // find current position
 $(window).on("scroll", function(){
   
@@ -25,10 +22,15 @@ $(window).on("scroll", function(){
     // user is scrolling up
     $navbar.removeClass('slideUp').addClass('slideDown');
   }
-    
-
   // Reset beforeScroll to afterScroll position
-  
   beforeScroll = afterScroll;
-  
+});
+
+// Skill bar component
+jQuery(document).ready(function(){
+  jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+      width:jQuery(this).attr('data-percent')
+    },3000);
+  });
 });
