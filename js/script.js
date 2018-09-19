eacd6acde745cec1e747a78f40a85d937fc688ba
adheel-barlas <adheelb8@gmail.com>
2018-09-19 16:41:48 +0100

    adding script for swiper

--- js/script.js
+++ js/script.js
@@ -69,3 +69,17 @@ $("#ctaProjects").click(function() {
       scrollTop: $("#projects").offset().top
   }, 2000);
 });
+
+var swiper = new Swiper('.blog-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  // autoHeight: true,
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
